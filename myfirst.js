aabc72512f58acbf57a0864a334685b320a6aa35
Kaustubh Chaudhari <124234164+kaustubhchaudhari05@users.noreply.github.com>
2024-05-13 22:41:38 +0530

    printing datetime

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./myfirstmodule')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write("the date and time are currentily: "+dt.myDateTime());
+  res.end();
 }).listen(8080);
